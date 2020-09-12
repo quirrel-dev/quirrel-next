ce1f55d078b27bdd170cbf33a3581a19e106f8fb
Simon Knott <info@simonknott.de>
2020-09-12 12:55:20 +0200

    Add QUIRREL_TOKEN env var support

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,21 @@
-export async function createJob(endpoint: string, runAt: Date | undefined, body: any) {
-  await fetch("https://api.quirrel.dev/jobs", {
+const baseUrl = process.env.QUIRREL_URL || "https://api.quirrel.dev";
+
+const token = process.env.QUIRREL_TOKEN;
+
+if (process.env.NODE_ENV === "production" && !token) {
+  throw new Error("Make sure to provide QUIRREL_TOKEN env var.");
+}
+
+export async function createJob(
+  endpoint: string,
+  runAt: Date | undefined,
+  body: any
+) {
+  await fetch(baseUrl + "/jobs", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify({
       endpoint,
