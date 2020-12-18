4a1853faa971393d2774a04f766cd88f879ebe7c
Simon Knott <info@simonknott.de>
2020-12-18 18:17:29 +0100

    Transition to the refactored Quirrel client

--- src/index.ts
+++ src/index.ts
@@ -6,95 +6,43 @@ import {
   DefaultJobOptions,
 } from "@quirrel/client";
 
-let baseUrl: string | undefined = undefined;
+export { Job, EnqueueJobOpts, DefaultJobOptions };
 
-if (process.env.VERCEL_URL) {
-  baseUrl = `https://${process.env.VERCEL_URL}/api/`;
-}
-
-if (process.env.QUIRREL_BASE_URL) {
-  baseUrl = `${process.env.QUIRREL_BASE_URL}/api/`;
-}
-
-if (!baseUrl) {
-  if (process.env.NODE_ENV === "production") {
-    throw new Error("Please specify QUIRREL_BASE_URL.");
-  } else {
-    baseUrl = "http://localhost:3000/api/";
-  }
-}
-
-const encryptionSecret = process.env.QUIRREL_ENCRYPTION_SECRET;
-if (process.env.NODE_ENV === "production") {
-  if (!encryptionSecret) {
-    throw new Error("Please specify `QUIRREL_ENCRYPTION_SECRET`.");
-  }
-
-  if (encryptionSecret.length !== 32) {
-    throw new Error("`QUIRREL_ENCRYPTION_SECRET` must have length 32.");
-  }
-}
-
-type Enqueue<Payload> = (
-  payload: Payload,
-  meta?: Omit<EnqueueJobOpts, "body">
-) => Promise<Job>;
-interface QueueResult<Payload> {
-  get(): AsyncIterator<Job[]>;
-  getById(id: string): Promise<Job | null>;
-  delete(id: string): Promise<boolean>;
-  invoke(id: string): Promise<boolean>;
-  enqueue: Enqueue<Payload>;
-}
+export type Queue<Payload> = Omit<QuirrelClient<Payload>, "respondTo">;
 
 export function Queue<Payload>(
-  path: string,
+  route: string,
   handler: (payload: Payload) => Promise<void>,
   defaultJobOptions?: DefaultJobOptions
-): QueueResult<Payload> {
-  const endpoint = baseUrl + path;
-
-  const quirrel = new QuirrelClient({
-    encryptionSecret,
+): Queue<Payload> {
+  const quirrel = new QuirrelClient<Payload>({
     defaultJobOptions,
+    handler,
+    route,
   });
 
   async function nextApiHandler(req: NextApiRequest, res: NextApiResponse) {
-    const { isValid, body } = quirrel.verifyRequestSignature<{
-      body: Payload;
-    }>(req.headers as any, req.body);
-    if (!isValid) {
-      return res.status(401).end();
+    const response = await quirrel.respondTo(
+      req.body,
+      req.headers["x-quirrel-signature"] as string
+    );
+    res.status(response.status);
+    for (const [header, value] of Object.entries(response.headers)) {
+      res.setHeader(header, value);
     }
-
-    const { body: payload } = body!;
-
-    console.log(`Received job to ${path}: `, payload);
-    try {
-      await handler(payload);
-      res.status(200).end();
-    } catch (error) {
-      res.status(500).json(error);
-      throw error;
-    }
+    res.send(response.body);
   }
 
-  nextApiHandler.enqueue = async (
-    payload: Payload,
-    meta?: Omit<EnqueueJobOpts, "body">
-  ) =>
-    quirrel.enqueue(endpoint, {
-      body: { body: payload },
-      ...meta,
-    });
+  nextApiHandler.enqueue = (payload: Payload, opts: EnqueueJobOpts) =>
+    quirrel.enqueue(payload, opts);
 
-  nextApiHandler.delete = (jobId: string) => quirrel.delete(endpoint, jobId);
+  nextApiHandler.delete = (jobId: string) => quirrel.delete(jobId);
 
-  nextApiHandler.invoke = (jobId: string) => quirrel.invoke(endpoint, jobId);
+  nextApiHandler.invoke = (jobId: string) => quirrel.invoke(jobId);
 
-  nextApiHandler.get = () => quirrel.get(endpoint);
+  nextApiHandler.get = () => quirrel.get();
 
-  nextApiHandler.getById = (jobId: string) => quirrel.getById(endpoint, jobId);
+  nextApiHandler.getById = (jobId: string) => quirrel.getById(jobId);
 
   return nextApiHandler;
 }
